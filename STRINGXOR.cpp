e71b86e230b5dc0e020f7cd214c00e12c93e0412
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-17 23:22:56 +0530

    Create STRINGXOR.cpp

---
+++ STRINGXOR.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main() {
+  ll t;
+  cin>>t;
+  while(t--)
+  {
+     ll n;
+     cin>>n;
+     string a,b;
+     cin>>a>>b;
+     size_t found=a.find('1');
+     if(n==2&&a=="00"&&b=="11")
+{
+    cout<<"NO"<<endl;
+}
+else
+{
+    bool h=false;
+    for(int i=1;i<n;i++)
+    {
+        if(b[i]==b[i-1])
+        {
+            h=true;
+            break;
+        }
+    }
+    if(h)
+    {
+        if(a==b)
+        {
+            cout<<"YES"<<endl;
+        }
+        else if(found==string::npos)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        if(a==b)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        cout<<"NO"<<endl;
+    }
+}
+  }
+  return 0;
+}
