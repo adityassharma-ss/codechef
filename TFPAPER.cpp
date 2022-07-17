2a02a8878bed7123e92874fb848eccc913308cd9
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-17 21:48:23 +0530

    Create TFPAPER.cpp

---
+++ TFPAPER.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int test;
+   cin >> test;
+   while(test--){
+      int n,k;
+      cin >> n >> k;
+      cout << n-k << endl;
+   }
+	// your code goes here
+	return 0;
+}
