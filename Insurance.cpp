986b7aff336c425d3c81935b5aa480cf5993fb13
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-07-23 15:37:53 +0530

    Create Insurance.cpp

---
+++ Insurance.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        if(y<=x){
+            cout << y << endl;
+        }
+        else{
+            cout << x << endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
